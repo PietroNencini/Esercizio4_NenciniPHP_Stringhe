02844c04ec0a204b6324094d8c4c7ea9c8427ef5
Pietro Nencini <nencinipietro2006@gmail.com>
2024-09-24 11:41:32 +0200

    First working commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Esercizio 4</title>
+</head>
+<body>
+    
+    <?php
+        $str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed blandit leo eleifend enim placerat, vitae auctor dolor vehicula. Morbi lobortis ipsum sit amet volutpat tincidunt. Nulla leo dolor, lobortis lobortis augue ac, auctor blandit nisl. Ut orci nunc, vehicula vel gravida et, commodo fermentum urna.";
+        
+        $par1 = "<p style='color:green;'> $str </p>";
+        $par2 = "<p style='color:blue;'> ".strtoupper($str)." </p>";
+        $par3 = "<span style='color:red;'> ".strlen($str)." </span><br>";
+        $par4 = "<span style='color:yellow; background-color: black'> ".str_word_count($str)."  </span>";
+        echo $par1, $par2, $par3, $par4;
+    ?>
+
+</body>
+</html>
