d41c7582e07e2d98ea973a5f8a4359c7e7c2ce1e
Pietro Nencini <nencinipietro2006@gmail.com>
2024-09-24 11:41:48 +0200

    Second working commit

--- index.php
+++ index.php
@@ -12,8 +12,8 @@
         
         $par1 = "<p style='color:green;'> $str </p>";
         $par2 = "<p style='color:blue;'> ".strtoupper($str)." </p>";
-        $par3 = "<span style='color:red;'> ".strlen($str)." </span><br>";
-        $par4 = "<span style='color:yellow; background-color: black'> ".str_word_count($str)."  </span>";
+        $par3 = "Numero di caratteri: <span style='color:red;'> ".strlen($str)." </span><br>";
+        $par4 = "Numero di parole: <span style='color:yellow; background-color: black'> ".str_word_count($str)."  </span>";
         echo $par1, $par2, $par3, $par4;
     ?>
 
